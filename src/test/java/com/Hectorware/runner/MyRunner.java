24537a9c561166c5e33a565d1fdda16ff068fc26
Timur Guclu <tmrguclu@hotmail.com>
2022-10-17 22:08:11 +0300

    Initial commit

---
+++ src/test/java/com/Hectorware/runner/MyRunner.java
@@ -0,0 +1,26 @@
+package com.Hectorware.runner;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                //"pretty",
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+//                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+        },
+        features = "src/test/resources/features",
+        glue = "com/Hectorware/stepDefinitions",
+        dryRun = false,
+        tags = "@Wip",
+        publish = false
+
+)
+public class MyRunner {
+
+
+}
+
