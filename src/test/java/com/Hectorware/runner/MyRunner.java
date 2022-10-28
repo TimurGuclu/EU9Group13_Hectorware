4c8860c8fcee526f462ec9a782869ac95267910b
Timur Guclu <tmrguclu@hotmail.com>
2022-10-28 22:00:00 +0300

    MyRunner modified

--- src/test/java/com/Hectorware/runner/MyRunner.java
+++ src/test/java/com/Hectorware/runner/MyRunner.java
@@ -6,15 +6,17 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+
         plugin = {
                 "pretty",
+                "json:target/cucumber.json",
                 "html:target/cucumber-report.html",
                 "rerun:target/rerun.txt",
 //                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
         },
         features = "src/test/resources/features",
         glue = "com/Hectorware/stepDefinitions",
-        dryRun = true,
+        dryRun = false,
         tags = "@Timur"      // unique tag names, leave blank
         //publish = false
 
