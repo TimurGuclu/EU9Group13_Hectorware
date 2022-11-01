906931a13883888a4837f5538d1b17d74d960221
Timur Guclu <tmrguclu@hotmail.com>
2022-11-01 20:48:47 +0300

    L

---
+++ src/test/java/com/Hectorware/stepDefinitions/LoginTimur_StepDef.java
@@ -0,0 +1,35 @@
+package com.Hectorware.stepDefinitions;
+
+import com.Hectorware.pages.LoginPageHalim;
+import com.Hectorware.pages.LoginPageTimur;
+import com.Hectorware.utilities.Driver;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class LoginTimur_StepDef {
+
+    LoginPageTimur loginPageTimur= new LoginPageTimur();
+    LoginPageHalim loginPageHalim= new LoginPageHalim();
+    @When("user enters valid username")
+    public void user_enters_valid_username() {
+        loginPageTimur.username.sendKeys("Employee300");
+    }
+    @When("user enters valid password")
+    public void user_enters_valid_password() {
+       loginPageTimur.password.sendKeys("Employee123");
+    }
+    @When("user clicks on login button")
+    public void user_clicks_on_login_button() {
+    loginPageTimur.loginBtn.click();
+    }
+    @Then("any user should verify the home page")
+    public void any_user_should_verify_the_home_page() {
+
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle= "Files - Hectorware - QA";
+        Assert.assertEquals(expectedTitle,actualTitle);
+
+    }
+
+}
