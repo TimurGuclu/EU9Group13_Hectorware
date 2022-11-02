d980da671775ae758e6d44980c6c7a14752304e9
NenadCvetic <cveticnenad92@gmail.com>
2022-11-03 00:15:57 +0100

    Created test case 2

---
+++ src/test/java/com/Hectorware/pages/TalkModulePage.java
@@ -0,0 +1,45 @@
+package com.Hectorware.pages;
+
+import com.Hectorware.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class TalkModulePage {
+
+
+    public TalkModulePage () {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+    @FindBy(xpath = "//button[@class='toggle has-tooltip']")
+    public WebElement plusSign;
+
+    @FindBy (xpath = "//input[@placeholder='Conversation name']")
+    public WebElement inputBox;
+
+    @FindBy (xpath = "//button[contains(@class,'navigation__button')]")
+    public WebElement addParticipantsButton;
+
+    @FindBy (xpath = "(//div[@class='participants-search-results scrollable']//div//ul//li)[1]")
+    public WebElement firstEmployeeButton;
+
+    @FindBy (xpath = "(//div[@class='participants-search-results scrollable']//div//ul//li)[2]")
+    public WebElement secondEmployeeButton;
+
+    @FindBy (xpath = "(//div[@class='navigation']//button)[2]")
+    public WebElement createConversationButton;
+
+    @FindBy (xpath = "//a[@aria-label='Conversation, avengersConversation']")
+    public WebElement avengersConversation;
+
+
+
+
+
+
+
+
+}
--- src/test/java/com/Hectorware/stepDefinitions/TalkFunctionality_StepDefinitions.java
+++ src/test/java/com/Hectorware/stepDefinitions/TalkFunctionality_StepDefinitions.java
@@ -2,16 +2,20 @@ package com.Hectorware.stepDefinitions;
 
 import com.Hectorware.pages.BasePage;
 import com.Hectorware.pages.LoginPageHalim;
+import com.Hectorware.pages.TalkModulePage;
 import com.Hectorware.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class TalkFunctionality_StepDefinitions {
 
     LoginPageHalim loginPageHalim = new LoginPageHalim();
 
     BasePage basePage = new BasePage();
+
+    TalkModulePage talkModulePage = new TalkModulePage();
 
     @Given("user is successfully logged in on Hectorware dashboard")
     public void user_is_successfully_logged_in_on_hectorware_dashboard() {
@@ -31,5 +35,45 @@ public class TalkFunctionality_StepDefinitions {
     @Then("Verify all conversation lists are displayed")
     public void verify_all_conversation_lists_are_displayed() {
 
+    }
+
+
+    @When("user clicks on plus sign to create new conversation")
+    public void user_clicks_on_plus_sign_to_create_new_conversation() {
+
+
+        talkModulePage.plusSign.click();
+
+    }
+    @When("user enters {string}")
+    public void user_enters(String string) {
+
+        talkModulePage.inputBox.sendKeys(string);
+
+    }
+    @When("user clicks on AddParticipants button")
+    public void user_clicks_on_add_participants_button() {
+
+        talkModulePage.addParticipantsButton.click();
+
+    }
+    @When("user selects participants he or she wants to add")
+    public void user_selects_participants_he_or_she_wants_to_add() {
+
+        talkModulePage.firstEmployeeButton.click();
+        talkModulePage.secondEmployeeButton.click();
+
+    }
+    @When("user clicks Create conversation button")
+    public void user_clicks_create_conversation_button() {
+
+        talkModulePage.createConversationButton.click();
+
+    }
+    @Then("verify {string} is created under the talk module")
+    public void verify_new_conversation_is_created_under_the_talk_module(String string) {
+
+
+        Assert.assertTrue(talkModulePage.avengersConversation.isDisplayed());
     }
 }
