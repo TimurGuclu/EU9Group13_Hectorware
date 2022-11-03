1e2d7413ad747da0a656ca2b391f6d5ac04ac673
Kaisar Ekfar <kaisar.ekfar@gmail.com>
2022-11-03 18:00:29 +0100

    updated

---
+++ src/test/java/com/Hectorware/stepDefinitions/FilesKaisar_Stepdefs.java
@@ -0,0 +1,22 @@
+package com.Hectorware.stepDefinitions;
+
+import com.Hectorware.pages.FilesPageKaisar;
+import com.Hectorware.pages.LoginPageTimur;
+
+public class FilesKaisar_Stepdefs {
+
+    LoginPageTimur login = new LoginPageTimur();
+    FilesPageKaisar files = new FilesPageKaisar();
+
+    /*
+        Driver.getDriver().get("https://qa.hectorware.com/index.php/login");
+
+        login.username.sendKeys("Employee8");
+
+        login.password.sendKeys("Employee123");
+
+        login.loginBtn.click();
+*/
+
+
+}
