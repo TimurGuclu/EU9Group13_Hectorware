cdaa8635beaee2e2d1ae1b7b141e31be1baa6b93
cfr2454 <cafercolak2454@gmail.com>
2022-11-20 15:30:14 +0300

    a new test case is created

--- src/test/java/com/Hectorware/stepDefinitions/NoteFunctionStepDefinitions.java
+++ src/test/java/com/Hectorware/stepDefinitions/NoteFunctionStepDefinitions.java
@@ -2,7 +2,9 @@ package com.Hectorware.stepDefinitions;
 
 import com.Hectorware.pages.LoginPageHalim;
 import com.Hectorware.pages.NotesPage;
+import com.Hectorware.utilities.BrowserUtils;
 import com.Hectorware.utilities.Driver;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
@@ -33,10 +35,57 @@ public class NoteFunctionStepDefinitions {
     @Then("user should verify writeable  field is displayed")
     public void user_should_verify_writeable_field_is_displayed() {
 
-        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 20);
-        wait.until(ExpectedConditions.visibilityOf(notesPage.codeMirror));
+        //WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 20);
+        //wait.until(ExpectedConditions.visibilityOf(notesPage.codeMirror));
 
         Assert.assertTrue( notesPage.codeMirror.isDisplayed());
+    }
+
+    @Given("user is on the notes page")
+    public void user_is_on_the_notes_page() {
+        Driver.getDriver().get("https://qa.hectorware.com/index.php/login");
+        //login.username.click();
+        login.username.sendKeys("Employee1");
+        //login.password.click();
+        login.password.sendKeys("Employee123");
+        login.LoginButton.click();
+        notesPage.notesIcon.click();
+
+    }
+    @When("user edits {string}")
+    public void user_edits(String string) {
+
+      notesPage.editibleCodeMirror.sendKeys(string);
+    }
+    @When("user clicks three dot on the left side of the page just on the right of Today is sunny note")
+    public void user_clicks_three_dot_on_the_left_side_of_the_page_just_on_the_right_of_today_is_sunny_note() {
+        BrowserUtils.scrollToElement(notesPage.todayIsSunny);
+        BrowserUtils.clickWithJS(notesPage.threeDot);
+
+    }
+    @When("user clicks rename button")
+    public void user_clicks_rename_button() {
+       notesPage.rename.click();
+    }
+    @When("user deletes the default name in the box")
+    public void user_deletes_the_default_name_in_the_box() {
+        BrowserUtils.waitForClickablility(notesPage.input,15 );
+        notesPage.input.click();
+        notesPage.input.clear();
+    }
+    @When("user edits the First Note in to the inbox")
+    public void user_edits_the_first_note_in_to_the_inbox() {
+      notesPage.input.sendKeys("First Note");
+    }
+    @When("user clicks arrow")
+    public void user_clicks_arrow() {
+        BrowserUtils.scrollToElement(notesPage.arrow);
+      BrowserUtils.clickWithJS(notesPage.arrow);
+    }
+    @Then("verify user views First Note on the page")
+    public void verify_user_views_first_note_on_the_page() {
+        BrowserUtils.waitForVisibility(notesPage.firstPage, 5);
+        Assert.assertTrue(notesPage.firstPage.isDisplayed());
     }
 
 
--- src/test/resources/features/NotesFunction.feature
+++ src/test/resources/features/NotesFunction.feature
@@ -1,11 +1,23 @@
 
 
 Feature: Notes Functionality
-  @cafer  @HECW-406
+ @HECW-406
   Scenario:User can view Notes module
     Given user is on the home page
     When user clicks notes icon
     And user clicks new note icon
     Then user should verify writeable  field is displayed
 
+
+  @cafer
+  Scenario: user can create a new note
+      Given user is on the notes page
+     When user clicks new note icon
+    And user edits "Today is sunny"
+    And user clicks three dot on the left side of the page just on the right of Today is sunny note
+    And user clicks rename button
+    And user deletes the default name in the box
+    And user edits the First Note in to the inbox
+    And user clicks arrow
+    Then verify user views First Note on the page
 
